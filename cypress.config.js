fe2bec0ec7bde57255d950df14f5f32b1d4b0270
Kaisa Piipari <kaisa.piipari@gmail.com>
2022-10-24 15:36:24 +0300

    Creates Cypress test suite for todo app

---
+++ cypress.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://mev0s6.csb.app/'
+  },
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,4 @@
+import '@testing-library/cypress/add-commands'
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
